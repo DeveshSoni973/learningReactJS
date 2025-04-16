971e4db6e4da2a7c3d97d2f04224504002afbc57
DeveshSoni973 <deveshsoni973@gmail.com>
2025-04-16 21:15:38 +0530

    learnt about doing custom render and then making own react element

--- 01fistapp/src/App.jsx
+++ 01fistapp/src/App.jsx
@@ -1,10 +1,14 @@
-import Nemo from "./nemo";
+import Nemo from "./Nemo";
 function App() {
-  
+
 
   return (
-    <Nemo/>
-    )
+    <> 
+    {/* the <> here and </> in the end ae used to wrap more than one tags in the return */}
+      <Nemo />
+      <h3>hello</h3>
+    </>
+  )
 }
 
 export default App;
--- 01fistapp/src/main.jsx
+++ 01fistapp/src/main.jsx
@@ -1,9 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+
+function DevBhaiOP() {
+  return (
+    <div>
+
+      <h1 className="devBhaiJi">Hello Cutie</h1>
+
+    </div>
+
+  )
+};
+
+
+// const ReactElement={ //making first letter captial thanks to the convention
+//   type:'a',
+//   props:{
+//       href:'https://google.com',
+//       target:'_blank'
+//   },
+//   children:'Click me to visit google'
+// } this will not work, fixing it below:
+
+const ReactElement=React.createElement(
+  'a',
+  {href:"https://google.com", target:"_blank"},
+  'click me to visit google'
+);
+
+
+const AnotherElement=(
+  <a href="https://google.com" target="_blank">Visit Google </a>
+);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   <App />
+  //   <DevBhaiOP />  {/* since it is a function, we can also do it like DevBhaiOP() function calling */}
+
+    // ReactElement
+    // AnotherElement
+    ReactElement
+  // </StrictMode>,
 )
